b5f2ac230a8d56ea263b53d070b3c26b13f8f0a6
Mathhman <minecraftmathhman@gmail.com>
2018-01-02 16:59:09 -0500

    Created a test for Loop.class.

--- src/test/java/com/meti/util/LoopTest.java
+++ src/test/java/com/meti/util/LoopTest.java
@@ -1,6 +1,9 @@
 package com.meti.util;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+
+import java.time.Duration;
 
 /**
  * @author SirMathhman
@@ -8,7 +11,48 @@ import org.junit.jupiter.api.Test;
  * @since 1/2/2018
  */
 class LoopTest {
+    private static final int[] data = new int[]{1, 2, 3};
+    private boolean success;
+
     @Test
-    public void construct() {
+    public void test() {
+        initTest();
+    }
+
+    private void initTest() {
+        Loop loop = new TestLoop(obj -> {
+            success = (obj != null);
+            return null;
+        });
+
+        Thread thread = new Thread(loop);
+        thread.start();
+
+        Assertions.assertTimeoutPreemptively(Duration.ofSeconds(5), () -> {
+            boolean next;
+            do {
+                next = success;
+            } while (!next);
+        });
+    }
+
+    private class TestLoop extends Loop {
+        int sum = 0;
+        int counter = 0;
+
+        TestLoop(ExceptionHandler handler) {
+            super(handler);
+        }
+
+        @Override
+        protected void loop() throws Exception {
+            sum += data[counter];
+
+            if (sum == 6) {
+                throw new Exception("Success!");
+            }
+
+            counter++;
+        }
     }
 }
