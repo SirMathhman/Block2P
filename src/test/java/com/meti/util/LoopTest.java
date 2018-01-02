95bd4fe2b14a3e11068a96a2f59781c1b7e76214
Mathhman <minecraftmathhman@gmail.com>
2018-01-02 16:48:26 -0500

    Modifying permissions so TravisCI can access Gradle, created a test for Loop.

---
+++ src/test/java/com/meti/util/LoopTest.java
@@ -0,0 +1,14 @@
+package com.meti.util;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * @author SirMathhman
+ * @version 0.0.0
+ * @since 1/2/2018
+ */
+class LoopTest {
+    @Test
+    public void construct() {
+    }
+}
