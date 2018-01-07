7b2f65e916bb86f11dd42cbd2a52ddc8ffe669a7
Mathhman <minecraftmathhman@gmail.com>
2018-01-07 16:35:07 -0500

    Resolved concurrency issues, moved tests into Handler1.

--- src/test/java/com/meti/io/buffer/SimpleBufferTest.java
+++ src/test/java/com/meti/io/buffer/SimpleBufferTest.java
@@ -7,12 +7,11 @@ import com.meti.io.connect.ConnectionHandler;
 import com.meti.io.connect.ConnectionListener;
 import com.meti.io.connect.connections.Connection;
 import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.RepeatedTest;
 
 import java.io.IOException;
 import java.net.InetAddress;
 import java.net.Socket;
-import java.time.Duration;
 
 /**
  * @author SirMathhman
@@ -20,10 +19,9 @@ import java.time.Duration;
  * @since 1/4/2018
  */
 class SimpleBufferTest {
-    private SimpleBuffer buffer1;
     private SimpleBuffer buffer2;
 
-    @Test
+    @RepeatedTest(10)
     public void test() throws IOException {
         ConnectionHandler handler1 = new Handler1();
         Peer peer1 = new Peer(handler1);
@@ -36,21 +34,23 @@ class SimpleBufferTest {
         ConnectionHandler handler2 = new Handler2();
         Peer peer2 = new Peer(handler2);
         peer2.initConnection(new Connection(socketSource));
-
-        Assertions.assertTimeoutPreemptively(Duration.ofSeconds(5), () -> {
-            buffer1.set(20, 100);
-
-            Assertions.assertEquals(100, buffer2.get(20));
-        });
     }
 
     private class Handler1 extends ConnectionHandler {
         @Override
         public Boolean handleImpl(Connection obj) {
-            buffer1 = new SimpleBuffer(obj);
-            buffer1.synchronize();
+            try {
+                SimpleBuffer buffer1 = new SimpleBuffer(obj);
+                buffer1.synchronize();
 
-            return true;
+                buffer1.set(20, 100);
+
+                Assertions.assertEquals(100, buffer2.get(20));
+                return true;
+            } catch (IOException e) {
+                e.printStackTrace();
+                return false;
+            }
         }
     }
 
