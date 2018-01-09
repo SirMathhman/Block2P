ab8ee91efb203d4fc1f9417377f531cab49d3d9e
Mathhman <minecraftmathhman@gmail.com>
2018-01-09 18:08:40 -0500

    Updated SimpleBufferTest to capture Exceptions in the Handlers.

--- src/main/java/com/meti/util/handle/BufferedExceptionHandler.java
+++ src/main/java/com/meti/util/handle/BufferedExceptionHandler.java
@@ -1,22 +1,31 @@
 package com.meti.util.handle;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.PriorityQueue;
+import java.util.Queue;
+
 /**
  * @author SirMathhman
  * @version 0.0.0
  * @since 1/9/2018
  */
 public class BufferedExceptionHandler extends ExceptionHandler {
-    private Exception mostRecent;
+    private final Queue<Exception> exceptionQueue = new PriorityQueue<>();
 
     //executed on exception thread
     @Override
     public Void handleImpl(Exception obj) {
-        mostRecent = obj;
+        exceptionQueue.add(obj);
         return null;
     }
 
     //executed on listener thread
     public Exception getMostRecent() {
-        return mostRecent;
+        return exceptionQueue.poll();
+    }
+
+    public List<Exception> asList() {
+        return Arrays.asList((Exception[]) exceptionQueue.toArray());
     }
 }
