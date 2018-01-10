38605020098fb36ad3aea7b650296b34c02150a3
Mathhman <minecraftmathhman@gmail.com>
2018-01-09 19:32:45 -0500

    Created the Manager class.

---
+++ src/main/java/com/meti/io/Manager.java
@@ -0,0 +1,16 @@
+package com.meti.io;
+
+import com.meti.io.buffer.ObjectBuffer;
+
+/**
+ * @author SirMathhman
+ * @version 0.0.0
+ * @since 1/9/2018
+ */
+public class Manager {
+    private final ObjectBuffer buffer;
+
+    public Manager(ObjectBuffer buffer) {
+        this.buffer = buffer;
+    }
+}
