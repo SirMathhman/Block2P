43fddb323ff7fc57e13adc2431961d45c0564b34
Mathhman <minecraftmathhman@gmail.com>
2018-01-12 19:59:08 -0500

    Completed documentation for the com.meti.io.buffer package

--- src/main/java/com/meti/io/buffer/SimpleBuffer.java
+++ src/main/java/com/meti/io/buffer/SimpleBuffer.java
@@ -3,6 +3,9 @@ package com.meti.io.buffer;
 import com.meti.io.connect.connections.ObjectConnection;
 
 /**
+ * <p>
+ *     SimpleBuffers can send back and forth bytes.
+ * </p>
  * @author SirMathhman
  * @version 0.0.0
  * @since 1/4/2018
